4171c2074ddc5aacc70f94653238404b62984833
Codinglone <codinglone@gmail.com>
2023-08-16 00:18:59 +0200

    Finished login component boilerplate

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,29 +1,43 @@
-import { StyleSheet, Text, SafeAreaView, View, TextInput } from "react-native";
+import { StyleSheet, Text, SafeAreaView, View, TextInput, Button } from "react-native";
 const Login = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
-      <View>
+      <View  style={styles.inputGroup}>
         <Text style={styles.labels}>Username</Text>
         <TextInput style={styles.field} />
+      </View>
+
+      <View style={styles.inputGroup}>
+        <Text style={styles.labels}>Password</Text>
+        <TextInput style={styles.field} />
+      </View>
+
+      <View style={styles.inputGroup}>
+        <Button title="Login" />
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        display: "flex"
+    },
     title: {
         fontWeight: 500,
         fontSize: "32px",
         color: "#fff",
-        paddingVertical: "30px",
+        paddingVertical: "50px",
         letterSpacing: "10px",
         fontFamily: "verdana"
     },
     labels: {
         color: "#fff",
         fontWeight: "550",
-        fontSize: "18px"
+        fontSize: "18px",
+        letterSpacing: "4px",
+        paddingBottom: "4px"
 
     }
     ,
@@ -35,6 +49,14 @@ const styles = StyleSheet.create({
         fontWeight: "500",
         fontSize: "18px"
 
+    },
+    inputGroup: {
+        paddingVertical: "20px"
+    },
+    button: {
+        paddingVertical: "16px",
+        color: "#fff",
+        backgroundColor: "000"
     }
 });
 
