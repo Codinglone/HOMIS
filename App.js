27c349bac8ee34361d87a517135bb0d502a6f875
Codinglone <codinglone@gmail.com>
2023-08-15 23:44:01 +0200

    Imported Login component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Title from './components/Title';
-
+import Login from './components/Login';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Title />
+      <Login />
       <StatusBar style="auto" />
     
     </SafeAreaView>
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, SafeAreaView, View, TextInput } from "react-native"
+import { StyleSheet, Text, SafeAreaView, View, TextInput } from "react-native";
 const Login = () => {
   return (
-    <View>Login</View>
-  )
-}
+    <View>
+      <Text>LOGIN</Text>
+      <View>
+        <Text>Username</Text>
+        <TextInput />
+      </View>
+    </View>
+  );
+};
 
+const styles = StyleSheet.create({});
 
-
-export default Login
+export default Login;
