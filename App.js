f022d406a06af22782e6577212c9424a21221280
Codinglone <codinglone@gmail.com>
2023-08-15 23:22:54 +0200

    Added basic styling

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={styles.title}
+      >HOMIS</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'teal',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  title: {
+    fontSize: "64px",
+    color: "#fff",
+    fontWeight: "900",
+    marginTop: "60px"
+  }
 });
