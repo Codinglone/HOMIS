11ae1f43d6ee9237609fdb8e1e0ed73220ef3e81
Codinglone <codinglone@gmail.com>
2023-08-16 08:17:36 +0200

    Added Client Order Copmponent

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Title from './components/Title';
 import Login from './components/Login';
+import ClientOrder from './components/ClientOrder';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Title />
-      <Login />
+      {/* <Login /> */}
+      <ClientOrder />
       <StatusBar style="auto" />
     
     </SafeAreaView>
---
+++ components/ClientOrder.jsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+
+const ClientOrder = () => {
+    return (
+        <View>
+            <Text>Client Order</Text>
+        </View>
+    )
+}
+
+
+export default ClientOrder
